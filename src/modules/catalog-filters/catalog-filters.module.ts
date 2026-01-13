7544daeddfdf54ccf8d1a003103879dd7b735b05
Artur <arturdudarev60@gmail.com>
2026-01-13 20:59:42 +0500

    Сделал каркас админки

--- src/modules/catalog-filters/catalog-filters.module.ts
+++ src/modules/catalog-filters/catalog-filters.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { CatalogFilter } from '@entities/Catalog/catalogFilters/catalog-filter.entity'
 import { CatalogFiltersController } from './catalog-filters.controller'
 import { CatalogFiltersService } from './catalog-filters.service'
-
+import { AdminCatalogFiltersController } from './admin/admin-catalog-filters.controller'
+import { AdminCatalogFiltersService } from './admin/admin-catalog-filters.service'
 @Module({
   imports: [TypeOrmModule.forFeature([CatalogFilter])],
-  controllers: [CatalogFiltersController],
-  providers: [CatalogFiltersService],
+  controllers: [CatalogFiltersController, AdminCatalogFiltersController],
+  providers: [CatalogFiltersService, AdminCatalogFiltersService],
 })
 export class CatalogFiltersModule {}
