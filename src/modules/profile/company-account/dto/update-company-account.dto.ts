0ee31ecd129f5ff394a9e53527233fd9442b7909
Artur <arturdudarev60@gmail.com>
2026-01-22 09:46:02 +0500

    Добавил сваггер

--- src/modules/profile/company-account/dto/update-company-account.dto.ts
+++ src/modules/profile/company-account/dto/update-company-account.dto.ts
@@ -1,30 +1,40 @@
+import { ApiPropertyOptional } from '@nestjs/swagger'
 import { IsOptional, IsString, IsUrl, IsObject } from 'class-validator'
 
 export class UpdateCompanyAccountDto {
+  @ApiPropertyOptional({ example: 'Тестовая компания' })
   @IsOptional()
   @IsString()
   name?: string
 
+  @ApiPropertyOptional({ example: 'Описание компании' })
   @IsOptional()
   @IsString()
   description?: string
 
+  @ApiPropertyOptional({ example: 'Челябинская область' })
   @IsOptional()
   @IsString()
   region?: string
 
+  @ApiPropertyOptional({ example: 'Строительство' })
   @IsOptional()
   @IsString()
   activity_type?: string
 
+  @ApiPropertyOptional({ example: 'https://example.com' })
   @IsOptional()
   @IsUrl()
   website?: string
 
+  @ApiPropertyOptional({ example: 'https://example.com/logo.png' })
   @IsOptional()
   @IsUrl()
   logo_url?: string
 
+  @ApiPropertyOptional({
+    example: { telegram: '@test', vk: 'vk.com/test' },
+  })
   @IsOptional()
   @IsObject()
   social_links?: Record<string, any>
