519d00494b71e2710124d2126efc4fa712ac997a
6ubble <bestartiom@yandex.ru>
2026-01-31 13:48:11 +0500

    mini fix для блогов

--- src/entities/Blog/blog-content.types.ts
+++ src/entities/Blog/blog-content.types.ts
@@ -5,6 +5,7 @@
 export type BlogContentBlock =
   | { type: "subtitle2"; text: string }
   | { type: "subtitle3"; text: string }
+  | { type: "paragraph"; text: string }
   | { type: "image"; url: string }
   | { type: "divider" }
   | { type: "bulletList"; items: string[] }
